822d82b50c9b25d541624135c3bf2ceea8264311
Kaira Aravena <kairaaravena@gmail.com>
2023-11-09 14:47:22 -0300

    arreglos para la ultima entrega

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { DatosPersonalesComponent } from './div/datospersonales/datospersonales.component';
-import { PerfilprofesionalComponent } from './fieldset/perfilprofesional/perfilprofesional.component';
-import { EstudiosacademicosComponent } from './fieldset/estudiosacademicos/estudiosacademicos.component';
-import { HabilidadesComponent } from './fieldset/habilidades/habilidades.component';
-import { ProyectosComponent } from './fieldset/proyectos/proyectos.component';
-import { ImagensiluetaComponent } from './div/imagensilueta/imagensilueta.component';
+import { DatosPersonalesComponent } from './componentes/datospersonales/datospersonales.component';
+import { PerfilprofesionalComponent } from './componentes/perfilprofesional/perfilprofesional.component';
+import { EstudiosacademicosComponent } from './componentes/estudiosacademicos/estudiosacademicos.component';
+import { HabilidadesComponent } from './componentes/habilidades/habilidades.component';
+import { ProyectosComponent } from './componentes/proyectos/proyectos.component';
+import { ImagensiluetaComponent } from './componentes/imagensilueta/imagensilueta.component';
 import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
